648d21a13815a04fd5d631a39b1be24d7140f3d3
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-12-08 11:42:35 -0300

    fix-req 03-entity-add table annotation to Parson entity.

--- src/main/java/com/betrybe/agrix/entity/Person.java
+++ src/main/java/com/betrybe/agrix/entity/Person.java
@@ -7,12 +7,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Objects;
 
 /**
  * Class representing a person.
  */
 @Entity
+@Table(name = "persons")
 public class Person {
 
   @Id
