d39546583457d239bbc6413517dd57dbc09f7b05
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-12-08 11:46:52 -0300

    feat-req 03-service-add dependencies injection to TokenService's constructor.

--- src/main/java/com/betrybe/agrix/service/TokenService.java
+++ src/main/java/com/betrybe/agrix/service/TokenService.java
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +20,6 @@ public class TokenService {
    *
    * @param secret the secret
    */
-  @Autowired
   public TokenService(@Value("${api.security.token.secret}") String secret) {
     this.algorithm = Algorithm.HMAC256(secret);
   }
